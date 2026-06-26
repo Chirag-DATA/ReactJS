0bbce0a65b3dbeffdd6dc4986d8cd00fdfa9608a
Chirag-DATA <chirag.personal.2004@gmail.com>
2026-06-26 14:26:14 +0530

    Day01 learnings

---
+++ learning_react/src/App.jsx
@@ -0,0 +1,20 @@
+// function App() {
+//   // return 'hello'
+//   return <h1>Hello World</h1>
+// }
+
+// export default App;
+
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello guys</h1>
+      <button>Hello ji!!</button>
+    </div>
+  )
+}
+
+export default App
